0bfd9ea376f6abedc939c9cef0d845683f29dd67
Arpan-jain001 <arpanjain00123@gmail.com>
2026-03-22 04:58:25 +0530

    fixed alias import issue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Toaster } from "@/components/ui/sonner";
-import { ToasterComponent } from "@/components/ui/toaster";
-import { TooltipProvider } from "@/components/ui/tooltip";
+import { Toaster } from "./components/ui/sonner";
+import { ToasterComponent } from "./components/ui/toaster";
+import { TooltipProvider } from "./components/ui/tooltip";
 import { useWarmup } from "./hooks/useWarmup.js";
 
 // Pages
